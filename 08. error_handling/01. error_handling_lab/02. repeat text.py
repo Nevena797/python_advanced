e66533f3feec70487edab934ff7c378f836b6af8
Nevena <nevena.rogova@gmx.at>
2024-10-28 12:02:40 +0100

    Add files via upload

---
+++ 08. error_handling/01. error_handling_lab/02. repeat text.py
@@ -0,0 +1,9 @@
+word = input()
+
+try:
+    times = int(input())
+
+except ValueError:
+    print("Variable times must be an integer")
+else:
+    print(word * times)
---
+++ 08. error_handling/02. error_handling_exer.py/test.py
@@ -0,0 +1,3 @@
+n = 5
+matrix = [input().split(", ") for _ in range(n)]
+print(matrix)
